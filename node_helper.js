df728cb7194790d5dbc9335bc18ca0357196a7db
fewieden <fewieden@htwg-konstanz.de>
2017-02-26 22:52:01 +0100

    add linter

--- node_helper.js
+++ node_helper.js
@@ -5,31 +5,33 @@
  * MIT Licensed.
  */
 
+/* eslint-env node */
+
+const NodeHelper = require('node_helper');
 const request = require('request');
-const NodeHelper = require("node_helper");
 
 module.exports = NodeHelper.create({
 
-    start: function() {
-        console.log("Starting module: " + this.name);
+    start() {
+        console.log(`Starting module: ${this.name}`);
     },
 
-    socketNotificationReceived: function(notification, payload) {
-        if(notification === 'CONFIG'){
+    socketNotificationReceived(notification, payload) {
+        if (notification === 'CONFIG') {
             this.config = payload;
-            this.getData({url: "http://www.medalbot.com/api/v1/medals"});
+            this.getData({ url: 'http://www.medalbot.com/api/v1/medals' });
             setInterval(() => {
-                this.getData({url: "http://www.medalbot.com/api/v1/medals"});
+                this.getData({ url: 'http://www.medalbot.com/api/v1/medals' });
             }, this.config.reloadInterval);
         }
     },
 
-    getData: function(options) {
+    getData(options) {
         request(options, (error, response, body) => {
             if (response.statusCode === 200) {
-                this.sendSocketNotification("MEDALS", JSON.parse(body));
+                this.sendSocketNotification('MEDALS', JSON.parse(body));
             } else {
-                console.log("Error getting olympic games medals " + response.statusCode);
+                console.log(`Error getting olympic games medals ${response.statusCode}`);
             }
         });
     }
