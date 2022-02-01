72fde0f8a969a06f4acce6e8c79be1724eea7769
Felix Wiedenbach <felix.wiedenbach@fleetster.net>
2022-02-01 21:42:47 +0100

    replace API

--- node_helper.js
+++ node_helper.js
@@ -7,37 +7,58 @@
 
 /* eslint-env node */
 
+const fetch = require('node-fetch');
+const _ = require('lodash');
+
 const NodeHelper = require('node_helper');
-const request = require('request');
+const Log = require('logger');
+
+const {getCountryAlpha2Code} = require('./countries');
+
+const DATA_URL = 'https://www.bloomberg.com/graphics/beijing-2022-olympics-data/current.json';
 
 module.exports = NodeHelper.create({
-
-    start() {
-        console.log(`Starting module: ${this.name}`);
-    },
+    requiresVersion: '2.15.0',
 
     socketNotificationReceived(notification, payload) {
         if (notification === 'CONFIG') {
-            this.config = payload;
-            this.getData({ url: 'https://www.bloomberg.com/graphics/2018-pyeongchang-olympics-medal-counter/live-data/total.json' });
+            this.getCountryMedals();
             setInterval(() => {
-                this.getData({ url: 'https://www.bloomberg.com/graphics/2018-pyeongchang-olympics-medal-counter/live-data/total.json' });
-            }, this.config.reloadInterval);
+                this.getCountryMedals();
+            }, payload.reloadInterval);
         }
     },
 
-    getData(options) {
-        request(options, (error, response, body) => {
-            if (response.statusCode === 200) {
-                let parsedBody = JSON.parse(body);
-                parsedBody.forEach((entry, index) => {
-                    entry.place = index;
-                });
-                parsedBody.shift();
-                this.sendSocketNotification('MEDALS', parsedBody);
-            } else {
-                console.log(`Error getting olympic games medals ${response.statusCode}`);
+    mapCountry(entry = {}, index) {
+        return {
+            rank: index + 1,
+            code: getCountryAlpha2Code(entry.noc),
+            gold: entry.gold,
+            silver: entry.silver,
+            bronze: entry.bronze,
+        };
+    },
+
+    async getCountryMedals() {
+        try {
+            const response = await fetch(DATA_URL, {
+                headers: {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36'
+                }
+            });
+
+            if (!response.ok) {
+                throw new Error('failed to fetch medals');
             }
-        });
+
+            const parsedResponse = await response.json();
+            const countries = _.get(parsedResponse, 'data.medals');
+            const sortedCountries = _.orderBy(countries, ['gold', 'silver', 'bronze', 'noc'], ['desc', 'desc', 'desc', 'asc']);
+            const mappedCountries = _.map(sortedCountries, this.mapCountry);
+
+            this.sendSocketNotification('COUNTRIES', mappedCountries);
+        } catch(e) {
+            Log.error('Error getting olympic game medals', e);
+        }
     }
 });
