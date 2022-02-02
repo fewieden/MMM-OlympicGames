35d715afabbc6fa842c21c0d5c7a425cb2353572
Kristjan SCHMIDT <35647502+KristjanESPERANTO@users.noreply.github.com>
2022-02-03 00:07:10 +0100

    Fix linter - Update eslint - Switch to 'eslint-config-airbnb' - Use 'prettier' - Rules revised

--- node_helper.js
+++ node_helper.js
@@ -5,23 +5,22 @@
  * MIT Licensed.
  */
 
-/* eslint-env node */
+const fetch = require("node-fetch");
+const _ = require("lodash");
 
-const fetch = require('node-fetch');
-const _ = require('lodash');
+const NodeHelper = require("node_helper");
+const Log = require("logger");
 
-const NodeHelper = require('node_helper');
-const Log = require('logger');
+const { getCountryAlpha2Code } = require("./countries");
 
-const { getCountryAlpha2Code } = require('./countries');
-
-const DATA_URL = 'https://www.bloomberg.com/graphics/beijing-2022-olympics-data/current.json';
+const DATA_URL =
+    "https://www.bloomberg.com/graphics/beijing-2022-olympics-data/current.json";
 
 module.exports = NodeHelper.create({
-    requiresVersion: '2.15.0',
+    requiresVersion: "2.15.0",
 
     socketNotificationReceived(notification, payload) {
-        if (notification === 'CONFIG') {
+        if (notification === "CONFIG") {
             this.getCountryMedals();
             setInterval(() => {
                 this.getCountryMedals();
@@ -35,7 +34,7 @@ module.exports = NodeHelper.create({
             code: getCountryAlpha2Code(entry.noc),
             gold: entry.gold,
             silver: entry.silver,
-            bronze: entry.bronze,
+            bronze: entry.bronze
         };
     },
 
@@ -43,22 +42,27 @@ module.exports = NodeHelper.create({
         try {
             const response = await fetch(DATA_URL, {
                 headers: {
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36'
+                    "User-Agent":
+                        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
                 }
             });
 
             if (!response.ok) {
-                throw new Error('failed to fetch medals');
+                throw new Error("failed to fetch medals");
             }
 
             const parsedResponse = await response.json();
-            const countries = _.get(parsedResponse, 'data.medals');
-            const sortedCountries = _.orderBy(countries, ['gold', 'silver', 'bronze', 'noc'], ['desc', 'desc', 'desc', 'asc']);
+            const countries = _.get(parsedResponse, "data.medals");
+            const sortedCountries = _.orderBy(
+                countries,
+                ["gold", "silver", "bronze", "noc"],
+                ["desc", "desc", "desc", "asc"]
+            );
             const mappedCountries = _.map(sortedCountries, this.mapCountry);
 
-            this.sendSocketNotification('COUNTRIES', mappedCountries);
+            this.sendSocketNotification("COUNTRIES", mappedCountries);
         } catch (e) {
-            Log.error('Error getting olympic game medals', e);
+            Log.error("Error getting olympic game medals", e);
         }
     }
 });
